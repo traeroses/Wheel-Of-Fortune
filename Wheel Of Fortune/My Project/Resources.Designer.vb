dd18d094c41942a5bdf6e6a7aa6c8c7ee69158d5
wyoung8861 <wyoung8861@W10-6RM2CS3>
2023-05-10 13:54:09 -0500

    new new

--- Wheel Of Fortune/My Project/Resources.Designer.vb
+++ Wheel Of Fortune/My Project/Resources.Designer.vb
@@ -73,6 +73,16 @@ Namespace My.Resources
         '''<summary>
         '''  Looks up a localized resource of type System.Drawing.Bitmap.
         '''</summary>
+        Friend ReadOnly Property w0() As System.Drawing.Bitmap
+            Get
+                Dim obj As Object = ResourceManager.GetObject("w0", resourceCulture)
+                Return CType(obj,System.Drawing.Bitmap)
+            End Get
+        End Property
+        
+        '''<summary>
+        '''  Looks up a localized resource of type System.Drawing.Bitmap.
+        '''</summary>
         Friend ReadOnly Property wheel_1() As System.Drawing.Bitmap
             Get
                 Dim obj As Object = ResourceManager.GetObject("wheel 1", resourceCulture)
